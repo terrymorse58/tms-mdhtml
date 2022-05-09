ebfd8e8c6d3a24da4beb137fdfc22a488b305cd3
Terry Morse <tmorse@terrymorse.com>
2022-05-09 15:39:47 -0700

    add meta to html template for viewport size

--- mdhtml.js
+++ mdhtml.js
@@ -10,6 +10,7 @@ const htmlTemplate =
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{title}}</title>
   <style>{{style}}</style>
 </head>
